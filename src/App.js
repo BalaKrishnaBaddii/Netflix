b82c4a1ebfe4609b52a39213b479262bf7bbf002
BalaKrishnaBaddii <bkrishna1114@gmail.com>
2024-06-22 18:07:40 +0530

    align ments done properly

--- src/App.js
+++ src/App.js
@@ -147,9 +147,27 @@ function BlocA() {
         </p>
         <div className="search-input-button">
           <input type="text" placeholder="Enter Address"></input>
-          <Button padding="0.6rem 1.5rem" fontSize="1rem">
+          <Button padding="0.55rem 1.5rem" fontSize="1rem">
             <p>Get Started</p>
-            <p>&#62;</p>
+            <p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                role="img"
+                data-icon="ChevronRightStandard"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M15.5859 12L8.29303 19.2928L9.70725 20.7071L17.7072 12.7071C17.8948 12.5195 18.0001 12.2652 18.0001 12C18.0001 11.7347 17.8948 11.4804 17.7072 11.2928L9.70724 3.29285L8.29303 4.70706L15.5859 12Z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </p>
           </Button>
         </div>
       </div>
